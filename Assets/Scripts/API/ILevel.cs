120ae830f53f7a7878696bd00330bfde3f0b7ce9
xkw9 <1577782034@qq.com>
2025-06-29 02:05:05 +0800

    add API

---
+++ Assets/Scripts/API/ILevel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.API
+{
+    public interface ILevel
+    {
+        public Vector2 GetPlayerOriginPos();
+
+        public OriginLocation GetDestination(MovingObject obj);
+
+        void LoadLevel(int level);
+
+        void Win();
+
+    }
+}
---
+++ Assets/Scripts/API/ILevelProgressUI.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.API
+{
+    public interface ILevelProgressUI
+    {
+
+        public void LoadLevelInfo(int level, int amountNeed);
+
+        public void RefreshCurAmount(int amount);
+
+        public void startCountDown(int seconds);
+
+    }
+}
